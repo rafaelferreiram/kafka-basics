51b050d15334262cc5e79510fa4c829936e1c6c8
rafael.ferreira <rafah.26ferreira@hotmail.com>
2020-04-07 22:24:04 -0300

    Creating Consumers

--- src/main/java/com/kafka/course/config/ConfigConstants.java
+++ src/main/java/com/kafka/course/config/ConfigConstants.java
@@ -4,6 +4,8 @@ public class ConfigConstants {
 
 	public static final String bootstrapServer = "127.0.0.1:9092";
 	public static final String fisrtTopic = "first_topic";
+	public static final String groupId = "my-java-consumer";
+	public static final String earliestOffset = "earliest";
 		
 
 }
